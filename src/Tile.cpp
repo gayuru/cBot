9f08aa9fa74d18644c379ffe7d2d9639e6a076d7
Minh <s3722599@student.rmit.edu.au>
2019-04-29 13:15:16 +1000

    tiles class should be good

--- src/Tile.cpp
+++ src/Tile.cpp
@@ -2,3 +2,29 @@
 #include "Tile.h"
 
 // Empty... for now?
+
+
+Tile::Tile(Colour colour, Shape shape) {
+    this->colour = colour;
+    this->shape = shape;
+}
+
+Colour Tile::getColour() {
+    return colour;
+}
+
+Shape Tile::getShape() {
+    return shape;
+}
+
+std::string Tile::toString() {
+    std::string thisString =  std::to_string(colour);
+    thisString += shape;
+    return thisString;
+
+}
+
+
+
+
+
--- src/Tile.h
+++ src/Tile.h
@@ -1,7 +1,7 @@
 
 #ifndef ASSIGN2_TILE_H
 #define ASSIGN2_TILE_H
-
+#include <string>
 // Define a Colour type
 typedef char Colour;
 
@@ -10,8 +10,16 @@ typedef int Shape;
 
 class Tile {
 public:
+   Tile(Colour colour, Shape shape);
+private:
    Colour colour;
    Shape  shape;
+
+   Colour getColour();
+   Shape getShape();
+   std::string toString();
+
+
 };
 
 #endif // ASSIGN2_TILE_H

b866d11e537bc27c24292d5137d4402fb3f9563d
Minh <s3722599@student.rmit.edu.au>
2019-05-21 16:45:49 +1000

    sfl.kaewgjkl;asjklfn.lwf

--- src/Board.h
+++ src/Board.h
@@ -14,19 +14,31 @@
 class Board
 {
 private:
+    //stores the direction
     int direction;
+    //Vector of Vector Board
     std::vector<std::vector<Tile *>> vBoard;
+    //Saves coordinates replaced by player, and clears once turn ends
     std::vector<Coordinate *> coordPlaced;
+    //Clearing the board
     void clearBoard();
+    //resizes the board
     void resizeBoard(int row, int col);
+    //validates the coordinates placed
     bool checkValidityV(int col, int row, Tile *tile);
+    //compares color and shape of tile and box
     bool colorShapeCheckV(Tile *tile, Tile *box, int fixType);
+    //checks for duplicates
     bool noDuplicateCheck(int min, int max, int row, int col, Tile *tile, bool rowCheck);
+    //finds the direction based on what you've placed
     void calculateDirection(int row, int col);
+    //resets the variables
     void refresh();
+    //validates the placed tile with the restricted direction
     bool directionCheck(int row, int col);
+    //checks if the tile is inbound
     Tile *inBoundCheck(int row, int col);
-    int calcPoints(int row, int col);
+    //sizes check
     int getVerticalRun(int row, int col);
     int getHorizontalRun(int row, int col);
 
@@ -35,10 +47,14 @@ public:
     ~Board();
     void printBoard();
     bool makeMoveV(char cRow, int col, Tile *tile);
+    //finalises points once turn ends
     int endPoints();
-    std::string getRow(int row);
+    //get the Horizontal size
     int getHSize();
+    //get the Vertical size
     int getVSize();
+    //for loading and saving
+    std::string getRow(int row);
     std::vector<Coordinate *> getCoordPlaced();
     void loadBoard(int rows, int cols, std::vector<Coordinate *> coords, std::vector<Tile *> tiles, std::vector<Coordinate *> coordOrder);
 };
